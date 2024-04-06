11199809e71e6905e32c8c0202969eb75e09ee6c
mobedi <p.mobedi@gmail.com>
2024-04-06 18:24:57 +0330

    6. start work with graphql

--- app/index.js
+++ app/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const http = require('http');
 const mongoose = require('mongoose');
+const { graphqlHTTP } = require('express-graphql');
+const {buildSchema} = require('graphql');
+
 const app = express();
 
 module.exports = class Application {
     constructor(){
         this.ServerConfig();
         this.DatabaseConfig();
+        this.Routes();
 
     }
     ServerConfig(){
@@ -19,6 +23,23 @@ module.exports = class Application {
     DatabaseConfig() {
         mongoose.Promise = global.Promise;
         mongoose.connect(config.database.url);
+    }
+
+    Routes() {
+        const schema = buildSchema (`
+            type Query {
+                user : String
+            }
+        `);
+        const resolver = {
+            user : () => {
+                return "ali"
+            }
+        }
+        app.use('/graphql', graphqlHTTP({
+            schema,
+            rootValue:resolver,
+        })); 
     }
 
 }
