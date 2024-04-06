4afb7a9c6eb5c3a91ef511ae3baa7897cc523b65
mobedi <p.mobedi@gmail.com>
2024-04-06 21:41:26 +0330

    10. register section 1

--- api/resolver.js
+++ api/resolver.js
@@ -6,6 +6,11 @@ const resolvers = {
                 return "ali"
             }
         },
+    Mutation: {
+        register : (param, args) => {
+                console.log(args);
+        }
+    }
   
 }
 
--- api/schema.js
+++ api/schema.js
@@ -4,7 +4,13 @@ const typeDefs = gql`
 type Query {
     user : String
 }
-
+type Mutation {
+    register(phone : String! , password : String! ) : operation!
+}
+type operation {
+    status : Int,
+    message : String
+}
 `;
 
 module.exports = typeDefs
