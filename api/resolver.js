39862ea5dd1ee5c7906502c42a8c2d61e781dbee
mobedi <p.mobedi@gmail.com>
2024-05-10 12:57:43 +0330

    19. upload file

--- api/resolver.js
+++ api/resolver.js
@@ -1,7 +1,11 @@
 const User = require('app/models/users');
+const mkdirp = require('mkdirp');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const Mutimedia = require('app/models/multimedia');
+const path = require('path');
+const fs = require('fs');
 const resolvers = {
     Query : {
         user : (param, args, { check }) => {
@@ -83,10 +87,13 @@ const resolvers = {
                     if(check && isAdmin){
                         let errors= [];
                             try{
-
+                                const { createReadStream, filename } = await args.image;
+                                const stream = createReadStream();
+                                const { filePath } = await saveImage({ stream, filename});
+            
                                 await Multimedia.create({
-                                    name ,
-                                    dir 
+                                    name : filename,
+                                    dir : filePath
                                 })
                                 return {
                                     status : 200,
@@ -109,5 +116,22 @@ const resolvers = {
     
         },
       }
-
+      let saveImage = ({ stream, filename}) => {
+        const date = new Date();
+        let dir = `/uploads/${date.getFullYear()}/${date.getMonth() + 1}`;
+        mkdirp.sync(path.join(__dirname, `/public/${dir}`))
+    
+        let filePath = `${dir}/${filename}`;
+    
+        if(fs.existsSync(path.join(__dirname,`/public/${filePath}`))) {
+            filePath = `${dir}/${Date.now()}-${filename}`
+        }
+    
+        return new Promise((resolve, reject) => {
+            stream
+                .pipe(fs.createWriteStream(path.join(__dirname, `/public/${filePath}`)))
+                .on('error', error => reject(error))
+                .on('finish', () => resolve({filePath}))
+        })
+    }
 module.exports = resolvers;
